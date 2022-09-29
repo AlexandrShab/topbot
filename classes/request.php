4475a63d28cd5f72b706620f095ca3923903e543
Alex Shab <alexandrshab@gmail.com>
2022-09-29 23:41:04 +0300

    main

--- classes/request.php
+++ classes/request.php
@@ -15,4 +15,20 @@ function sendDataToBot($postData)
     $context = stream_context_create($aHTTP);
     file_get_contents($BOT_URL, false, $context);
     return true;
-}   
+} 
+function getTelegramUserData() {
+    if (isset($_COOKIE['tg_user'])) {
+      //include __DIR__ . '/api/class_user.php';
+      $auth_data_json = urldecode($_COOKIE['tg_user']);
+      $auth_data = json_decode($auth_data_json, true);
+      $user = new User($auth_data);
+      if(!$user->isInBase())
+      {
+          $user->addTobase();
+      }
+      $user->checkAdmin();
+  
+      return $user;
+    }
+    return false;
+  }
