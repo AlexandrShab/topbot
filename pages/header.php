ac143b0c64ced52bf4755b00893158e35a5332a9
Alex Shab <alexandrshab@gmail.com>
2023-04-19 01:48:48 +0300

    new spin

--- pages/header.php
+++ pages/header.php
@@ -9,7 +9,96 @@
     <title>Belarus🔝Team</title>
     <!--?php echo "<title>$title</title>" ?> -->
     <style>
+    
+        .loading {
+            visibility: hidden;
+            display: flex;
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            background-color: rgba(20, 20, 20, 0.7);
+            align-items: center;
+            justify-content: center;
+            width: 100%;
+            height: 100%;
+            z-index: 20;
+            transition: 0.2s;
+        }
+
         .spinner {
+        transform: rotateZ(45deg);
+        perspective: 1000px;
+        border-radius: 50%;
+        width: 48px;
+        height: 48px;
+        color: #fff;
+      }
+        .spinner:before,
+        .spinner:after {
+          content: '';
+          display: block;
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: inherit;
+          height: inherit;
+          border-radius: 50%;
+          transform: rotateX(70deg);
+          animation: 1s spin linear infinite;
+        }
+        .spinner:after {
+          color: #FF3D00;
+          transform: rotateY(70deg);
+          animation-delay: .4s;
+        }
+
+      @keyframes rotate {
+        0% {
+          transform: translate(-50%, -50%) rotateZ(0deg);
+        }
+        100% {
+          transform: translate(-50%, -50%) rotateZ(360deg);
+        }
+      }
+
+      @keyframes rotateccw {
+        0% {
+          transform: translate(-50%, -50%) rotate(0deg);
+        }
+        100% {
+          transform: translate(-50%, -50%) rotate(-360deg);
+        }
+      }
+
+      @keyframes spin {
+        0%,
+        100% {
+          box-shadow: .2em 0px 0 0px currentcolor;
+        }
+        12% {
+          box-shadow: .2em .2em 0 0 currentcolor;
+        }
+        25% {
+          box-shadow: 0 .2em 0 0px currentcolor;
+        }
+        37% {
+          box-shadow: -.2em .2em 0 0 currentcolor;
+        }
+        50% {
+          box-shadow: -.2em 0 0 0 currentcolor;
+        }
+        62% {
+          box-shadow: -.2em -.2em 0 0 currentcolor;
+        }
+        75% {
+          box-shadow: 0px -.2em 0 0 currentcolor;
+        }
+        87% {
+          box-shadow: .2em -.2em 0 0 currentcolor;
+        }
+      }
+   
+/*         .spinner {
           width: 40px;
           height: 98px;
           display: inline-block;
@@ -49,23 +138,10 @@
 
  
 
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-        .loading {
-            visibility: hidden;
-            display: flex;
-            position: fixed;
-            top: 0px;
-            left: 0px;
-            background-color: rgba(20, 20, 20, 0.7);
-            align-items: center;
-            justify-content: center;
-            width: 100%;
-            height: 100%;
-            z-index: 20;
-            transition: 0.2s;
-        }
 
-/*        .spinner {
+        
+
+       .spinner {
 
             height: 100px;
             width: 100px;
